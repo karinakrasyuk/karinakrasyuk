85ef60cc13a6c9012dffaac875ae354b80ec0add
karinakrasyuk <113038313+karinakrasyuk@users.noreply.github.com>
2022-10-05 22:45:25 +0300

    Update vigenere.py

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -1,3 +1,5 @@
+from caesar import encrypt_caesar
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -10,29 +12,18 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     ciphertext = ""
     key = keyword
-    while len(key) < len(plaintext):   
+    while len(key) < len(plaintext):
         key += keyword
 
     for i in range(0, len(plaintext)):
-        c = plaintext[i]   
-        shift = None       
-        if key[i].isupper():    
+
+        if key[i].isupper():
             shift = ord(key[i]) - ord('A')
         else:
             shift = ord(key[i]) - ord('a')
 
-        if c.isalpha():  
-            mesto = ord(c)
-            if c.isupper():  
-                k = ord('A')
-            else:
-                k = ord('a')
+        ciphertext += encrypt_caesare(ciphertext[i], shift)
 
-            new_mesto = k + (mesto + shift - k) % 26
-            ciphertext += chr(new_mesto)
-        else:
-            ciphertext += c  
-            
     return ciphertext
 
 
@@ -52,25 +43,13 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
         key += keyword
 
     for i in range(0, len(ciphertext)):
-        c = ciphertext[i]
 
-        shift = None
+
         if key[i].isupper():
             shift = ord(key[i]) - ord('A')
         else:
             shift = ord(key[i]) - ord('a')
 
+        plaintext += encrypt_caesare(ciphertext[i], -shift)
 
-        if c.isalpha():  
-            mesto = ord(c)
-            if c.isupper():  
-                k = ord('A')
-            else:
-                k = ord('a')
-
-            new_mesto = k + (mesto - shift - k + 26) % 26
-            plaintext += chr(new_mesto)
-        else:
-            plaintext += c  
-    
     return plaintext
